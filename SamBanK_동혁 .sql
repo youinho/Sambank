f1a8d1c06d00672642e7d6ee93b2af2cb704ae73
ucceoum <ucceoum@gmail.com>
2020-05-10 02:04:42 +0900

    오류 수정

--- SamBanK_동혁 .sql
+++ SamBanK_동혁 .sql
@@ -640,4 +640,7 @@ select * from customertbl order by cno desc;
 
 select * from customer_inquiry where expdate<sysdate;
 
+
+select * from customertbl where id='rlaehdgur115';
+select * from locked_customer;
 -- <Connector SSLEnabled="true" keystoreFile="d:/SamBank.keystorsee" keystorePass="123456" port="8443" scheme="https" secure="true" sslProtocol="TLS" sslEnabledProtocols="TLSv1.2,TLSv1.1,TLSv1,SSLv2Hello"/>
