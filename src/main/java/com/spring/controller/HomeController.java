32f75a071782aee6d55341e8b1b4e3cdc0586d1f
ucceoum <ucceoum@gmail.com>
2020-05-11 00:04:07 +0900

    ㅎㅎ...

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.spring.controller;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -269,7 +272,105 @@ public class HomeController {
 	
 	
 	
+	//umbrella
 	
+	@GetMapping("/umbrella")
+	public String umbrella() {
+//		log.info("umbrella 페이지 요청");
+		
+		return "umbrella2";
+	}
+	
+	@PostMapping("/umbrella/save_center")
+	public void umbrella_save_center(String center_lat, String center_lng, String user_uuid) {
+		
+		File file = new File("/home/ec2-user/umbrella/"+user_uuid+"center.txt");
+		FileWriter writer = null;
+		
+		try {
+			writer = new FileWriter(file, false);
+			writer.write(center_lat+" "+center_lng);
+			writer.flush();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(writer != null) {
+					writer.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		System.out.println("saveCenter_done");
+	}
+	
+	@PostMapping("/umbrella/get_center")
+	@ResponseBody
+	public String umbrella_get_center(String user_uuid) {
+		String latlng = "";
+		File file = new File("/home/ec2-user/umbrella/"+user_uuid+"center.txt");
+		try(FileReader filereader = new FileReader(file);
+			BufferedReader br = new BufferedReader(filereader)
+			) {
+			latlng = br.readLine();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+		System.out.println("getCenter_done"+latlng);
+		return latlng;
+	}
+
+	
+	@PostMapping("/umbrella/save_level")
+	public void umbrella_save_level(String level, String user_uuid) {
+		
+		File file = new File("/home/ec2-user/umbrella/"+user_uuid+"level.txt");
+		FileWriter writer = null;
+		
+		try {
+			writer = new FileWriter(file, false);
+			writer.write(level);
+			writer.flush();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(writer != null) {
+					writer.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+	}
+	
+	@PostMapping("/umbrella/get_level")
+	@ResponseBody
+	public String umbrella_get_level(String user_uuid) {
+		String level = "";
+		File file = new File("/home/ec2-user/umbrella/"+user_uuid+"level.txt");
+		try(FileReader filereader = new FileReader(file);
+			BufferedReader br = new BufferedReader(filereader)
+			) {
+			level = br.readLine();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return level;
+	}
 	
 
 }
